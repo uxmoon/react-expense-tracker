9f4417fcd6baef6bc31db660a12e436004cbfe6e
Martin Luna <uxmoon@gmail.com>
2021-07-30 00:03:02 -0300

    Render list of data with array map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,26 +17,8 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selectedFilter={filterData} onSaveFilterData={handleFilterData} />
-        <ExpenseItem
-          title={props.data[0].title}
-          date={props.data[0].date}
-          amount={props.data[0].amount}
-        />
-        <ExpenseItem
-          title={props.data[1].title}
-          date={props.data[1].date}
-          amount={props.data[1].amount}
-        />
-        <ExpenseItem
-          title={props.data[2].title}
-          date={props.data[2].date}
-          amount={props.data[2].amount}
-        />
-        <ExpenseItem
-          title={props.data[3].title}
-          date={props.data[3].date}
-          amount={props.data[3].amount}
-        />
+        {props.data.map(expense => <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount} />)}
+
       </Card>
     </div>
   );
